ee9ca6c051a108fc29303930790fdf9f15f491e3
akramfaisal <akramfaisal@gmail.com>
2015-09-22 11:47:57 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the R file written for the Programming Assignment 2:
+## Lexical Scoping of R Programming Course on Coursera.
+## Name: Akram Faisal
+## Email: akramfaisal@gmail.com
 
-## Write a short comment describing this function
+## The task is to create a matrix inverse caching function. Inverse
+## of a matrix is usually costly to compute. Therefore, is the
+## matrix does not change, and if inverse is needed more than once,
+## it might be a good idea to cache the inverse.
+## NOTE: Caching sample code for mean of a vector was given,
+##       therefore, will have similarity to it.
+## NOTE 2: For this assignment, we are told to assume that the matrix
+##         supplied is always invertible.
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
